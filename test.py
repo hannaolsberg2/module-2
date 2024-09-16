ab46937731b377a808e1cb9fbbba9c61c56ad68a
hannaolsberg2 <hanna.olsberg@gmail.com>
2024-09-16 19:29:40 +0700

    day6

--- test.py
+++ test.py
@@ -1,1 +1,41 @@
-print('hello world')
+#print('hello world') 
+# Input
+#name_input = input()
+#float integer and convert any float number or string of float and true or false
+#true=1
+#str() will convert anything
+#lecture day 4
+#and, or, not  not(True) = false  / 2>3 and 3>1 false and true = false
+#never do operations in functions
+#first_number = int(input())
+#second_number = int(input())
+#added_result = first_number + second_number
+#print(added_result)
+#task: convert celsius to farenhiet
+#celsius = int(input())
+#fahrenheit = (celsius * 9/5) + 32
+#print(fahrenheit)
+#input value will always be string, so it was be converted into int ot float
+
+
+
+
+
+
+#number = 1 
+#while number <= 20:
+   # print(number)
+    #number += 1
+
+## for loop
+#range(start,stop,step)
+
+#'stop'
+#range(6) 0,1,2,3,4,5
+#'start and stop'
+#range(2,6) 2,3,4,5
+#range(5,2) nothing
+
+1,2,3,4,5,6,7,8,9,10
+for i in range(1,11):
+    print(i ** 2)
